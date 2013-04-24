694d66ef6c0f9e46299ce1a2d19d87f3fe5c39e8
Julien Fusco <pkoin.koin@gmail.com>
2013-04-24 21:18:43 +0200

    Add: rails new --database=mysql --skip-sprockets --skip-javascript --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApiScrumRb::Application.config.secret_key_base = 'de5b37b1518cb990117bf8a82a62f99d0cbe719b38ccea266e96bdf100b2675d4a72567f005952603287bcb282109d0b43621ac486031df3135bdf38faf0a95c'
