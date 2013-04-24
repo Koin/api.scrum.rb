415f005ae8dde368495314f2cc64567933016566
Julien Fusco <pkoin.koin@gmail.com>
2013-04-24 22:09:44 +0200

    Add: rails g jbuilder_scaffold_controller product label:string description:text

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :label, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -46,4 +46,5 @@ ApiScrumRb::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  resources :products
 end
