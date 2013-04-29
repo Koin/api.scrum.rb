c874b9962a13db940d455843904becf27b8a589c
Julien Fusco <pkoin.koin@gmail.com>
2013-04-29 21:11:12 +0200

    Mod: rake rails:update

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ApiScrumRb::Application.config.secret_key_base = 'de5b37b1518cb990117bf8a82a62f99d0cbe719b38ccea266e96bdf100b2675d4a72567f005952603287bcb282109d0b43621ac486031df3135bdf38faf0a95c'
+ApiScrumRb::Application.config.secret_key_base = '0552b1b632d45ddce5ca0ff272f00991f8b86b9b4a2e75dfa4f59f46a14526468669789b9bd7e9ddaef8ab1ead3ac18549b100102226d8d0457d7befdec50b6e'
