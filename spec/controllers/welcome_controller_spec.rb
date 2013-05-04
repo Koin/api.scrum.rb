ed7e4930f593727d27404ecb0ce13eb8c69b9deb
Julien Fusco <pkoin.koin@gmail.com>
2013-05-04 11:31:31 +0200

    Mod: test welcome#index

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe WelcomeController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
       response.should be_success
+      response.code.should eq "200"
     end
   end
 
