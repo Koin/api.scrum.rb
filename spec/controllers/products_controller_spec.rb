c189f227dff53de7b3ddf765ab0c316446755de0
Julien Fusco <pkoin.koin@gmail.com>
2013-06-11 23:47:37 +0200

    Mod: products controller spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,11 +1,28 @@
 require "spec_helper"
 describe ProductsController do
 
+  render_views
+
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
+      FactoryGirl.create_list :product, 10
       get :index
       response.should be_success
       response.code.should eq "200"
+    end
+  end
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      product = FactoryGirl.create :product
+      get :show, :id => product.id
+      response.should be_success
+      response.code.should eq "200"
+    end
+    it "responds with a 404 status code if the product doesn't exist" do
+      get :show, :id => -1
+      response.should_not be_success
+      response.code.should eq "404"
     end
   end
 
@@ -30,7 +47,7 @@ describe ProductsController do
       response.code.should eq "204"
     end
     it "responds with a 404 status code if the product doesn't exist" do
-      patch :update, :id => 7777, :product => { :label => "My unknown project" }
+      patch :update, :id => -1, :product => { :label => "My unknown project" }
       response.should_not be_success
       response.code.should eq "404"
     end
@@ -42,6 +59,11 @@ describe ProductsController do
       delete :destroy, :id => product.id
       response.should be_success
       response.code.should eq "204"
+    end
+    it "responds with a 404 status code if the product doesn't exist" do
+      delete :destroy, :id => -1
+      response.should_not be_success
+      response.code.should eq "404"
     end
   end
 end
